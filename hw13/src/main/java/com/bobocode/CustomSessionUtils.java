7716e23b83e8ab408f162d16aa937e5d696d5ee5
Bogdan Handozhynskyi <bk@gmail.com>
2022-06-27 21:38:24 -0700

    Homework#13: create the EveningServlet and simple custom session

---
+++ hw13/src/main/java/com/bobocode/CustomSessionUtils.java
@@ -0,0 +1,64 @@
+package com.bobocode;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.experimental.UtilityClass;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@UtilityClass
+public class CustomSessionUtils {
+
+    private final String CUSTOM_SESSION_ID = "CUSTOM_SESSION_ID";
+    private final ConcurrentMap<String, Map<String, String>> SESSION_MAP = new ConcurrentHashMap<>();
+
+    /**
+     * Find an attribute value in the session if such session and attribute name exist.
+     *
+     * @param req  incoming HTTP request
+     * @param name session attribute name to look up
+     * @return an {@code Optional} containing the value of for the attribute or empty {@code Optional
+     */
+    public Optional<String> getAttribute(HttpServletRequest req, String name) {
+        return getSessionId(req)
+                .map(SESSION_MAP::get)
+                .map(attributes -> attributes.get(name));
+    }
+
+    private Optional<String> getSessionId(HttpServletRequest req) {
+        return findCookieByName(req.getCookies(), CUSTOM_SESSION_ID)
+                .map(Cookie::getValue);
+    }
+
+    private Optional<Cookie> findCookieByName(Cookie[] cookies, String cookieName) {
+        if (cookies == null) {
+            return Optional.empty();
+        }
+
+        return Arrays.stream(cookies)
+                .filter(c -> c.getName().equalsIgnoreCase(cookieName))
+                .findAny();
+    }
+
+    /**
+     * Set a new or update an existing attribute in the existing session using provided name and value.
+     * If no session exists, a new session is created with given attribute
+     * and new {@value #CUSTOM_SESSION_ID} value is set as a cookie in the response
+     *
+     * @param req  current request
+     * @param resp current response
+     * @param name attribute name
+     * @param val  attribute value
+     */
+    public void setAttribute(HttpServletRequest req,
+                             HttpServletResponse resp,
+                             String name, String val) {
+        String sessionId = getSessionId(req).orElseGet(() -> UUID.randomUUID().toString());
+        SESSION_MAP.computeIfAbsent(sessionId, id -> new HashMap<>()).put(name, val);
+
+        resp.addCookie(new Cookie(CUSTOM_SESSION_ID, sessionId));
+    }
+}
---
+++ hw13/src/main/java/com/bobocode/EveningServlet.java
@@ -0,0 +1,33 @@
+package com.bobocode;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/evening")
+public class EveningServlet extends HttpServlet {
+
+    private static final String NAME_QUERY_PARAM = "name";
+    private static final String DEFAULT_NAME = "buddy";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (var writer = resp.getWriter()) {
+            var queryParamValue = Optional.ofNullable(req.getParameter(NAME_QUERY_PARAM));
+
+            String name = queryParamValue
+                    .or(() -> CustomSessionUtils.getAttribute(req, NAME_QUERY_PARAM))
+                    .orElse(DEFAULT_NAME);
+
+            if (queryParamValue.isPresent()) {
+                CustomSessionUtils.setAttribute(req, resp, NAME_QUERY_PARAM, name);
+            }
+
+            writer.printf("Good evening, %s", name);
+        }
+    }
+}
